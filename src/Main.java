dbc4dc2e97bd8f2063412d3aedca7ae281ac94f0
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2023-09-20 16:03:15 +0200

    läste lite om klasser och objekt och metoder på w3schools och prövade att använda det

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+class Game {
+    int health = 50;
+    int damage = (int)(Math.random()*10);
+}
+
 public class Main {
+    static int fight(int playerHealth, int enemyDamage){
+        playerHealth = playerHealth-enemyDamage;
+        return playerHealth;
+    }
+    static void damageCallout(int damage){
+        System.out.println("Enemy attacks for "+damage+" health.");
+    }
+    static void healthCallout(int health){
+        System.out.println("The player has "+health+" health remaining.");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Game player = new Game();
+        Game enemy = new Game();
+        damageCallout(enemy.damage);
+        player.health = fight(player.health, enemy.damage);
+        healthCallout(player.health);
     }
 }
