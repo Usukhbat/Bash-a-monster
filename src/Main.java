a5f7ea87685acef043e456369e20bf3060ea768a
Usukh Batkhuu <usukh-ireedui.batkhuu@elev.ga.ntig.se>
2023-09-26 22:29:15 +0200

    läste lite om constructurs och användde dem i min kod. Gjorde två styckna constructors.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
-class Game {
-    int health = 50;
-    int damage = (int)(Math.random()*10);
+class Characters {
+    int health;
+    public Characters() {
+        health = 50;
+    }
 }
-
+class Weapons {
+    int damage;
+    public Weapons() {
+        damage = (int)(Math.random()*10);
+    }
+}
 public class Main {
+    public static void main(String[] args) {
+        Characters player = new Characters();
+        Characters enemy = new Characters();
+        Weapons sword = new Weapons();
+        damageCallout(sword.damage);
+        player.health = fight(player.health, sword.damage);
+        healthCallout(player.health);
+    }
     static int fight(int playerHealth, int enemyDamage){
         playerHealth = playerHealth-enemyDamage;
         return playerHealth;
@@ -13,12 +28,5 @@ public class Main {
     }
     static void healthCallout(int health){
         System.out.println("The player has "+health+" health remaining.");
-    }
-    public static void main(String[] args) {
-        Game player = new Game();
-        Game enemy = new Game();
-        damageCallout(enemy.damage);
-        player.health = fight(player.health, enemy.damage);
-        healthCallout(player.health);
     }
 }
